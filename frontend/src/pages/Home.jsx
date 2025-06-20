1c7494eb50be196d5c1bf944dbc4ccdd26f935f2
KuldeepDawar <kuldeepdawar47@gmail.com>
2025-06-20 12:33:20 +1000

    Created best seller page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../compoenents/Hero";
 import LatestCollection from "../compoenents/LatestCollection";
+import BestSellere from "../compoenents/BestSellere";
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <LatestCollection />
+      <BestSellere />
     </div>
   );
 };
